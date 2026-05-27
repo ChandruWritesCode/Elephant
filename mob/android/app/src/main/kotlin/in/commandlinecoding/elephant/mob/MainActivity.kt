b4351ca2a2da06aa45b841dc484b5ef0c202bca1
Suman Biswas <sumanbiswas25bcs78@iiitkottayam.ac.in>
2026-05-27 13:39:12 +0530

    v0.1.0 init

---
+++ mob/android/app/src/main/kotlin/in/commandlinecoding/elephant/mob/MainActivity.kt
@@ -0,0 +1,5 @@
+package `in`.commandlinecoding.elephant.mob
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ server/go.mod
@@ -0,0 +1,3 @@
+module github.com/commandlinecoding/elephant/server
+
+go 1.26.3
